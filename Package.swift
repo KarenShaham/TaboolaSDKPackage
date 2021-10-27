20139a57a68de5dcbd706152901bc4eb807bfb8a
Karen Shaham Palman <58982236+KarenShaham@users.noreply.github.com>
2021-10-27 22:04:20 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "TaboolaSDKLibrary", dependencies: ["TaboolaSDK"]), //TODO: Delete later
         .binaryTarget(name: "TaboolaSDK",
-                      url: "https://github.com/KarenShaham/TBLXCFramework"),
+                      url: "https://github.com/KarenShaham/TBLXCFramework.framework"),
         .testTarget(
             name: "TaboolaSDKLibraryTests",
             dependencies: ["TaboolaSDKLibrary"]),
