c34be30f9c638adf6ce640100ce65281a27fff11
Karen Shaham Palman <58982236+KarenShaham@users.noreply.github.com>
2021-10-28 11:55:56 +0300

    Add files via upload

---
+++ Sources/ModuleX-ObjC/TaboolaSDKFramework.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  
+//
+//  Created by Karen Shaham Palman on 28/10/2021.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import <TaboolaSDK/TaboolaSDK.h>
+
+#endif /* Header_h */
