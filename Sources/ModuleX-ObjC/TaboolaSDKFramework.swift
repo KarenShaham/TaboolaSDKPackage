7ff1b4be85266b75493c7521585dab83d7bd5583
Karen Shaham Palman <58982236+KarenShaham@users.noreply.github.com>
2021-10-28 12:19:45 +0300

    Update TaboolaSDKFramework.swift

--- Sources/ModuleX-ObjC/TaboolaSDKFramework.swift
+++ Sources/ModuleX-ObjC/TaboolaSDKFramework.swift
@@ -1,10 +1,28 @@
 //
-//  File.swift
+//  TaboolaSDKFramework.swift
 //  
 //
 //  Created by Karen Shaham Palman on 28/10/2021.
 //
 
-import Foundation
+import UIKit
 import TaboolaSDK
 
+class TaboolaSDKFramework: UIViewController, TBLClassicPageDelegate {
+    var classicPage: TBLClassicPage?
+    var taboolaWidgetPlacement: TBLClassicUnit?
+    var taboolaFeedPlacement: TBLClassicUnit?
+   
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        taboolaInit()
+    }
+        
+    func taboolaInit(){
+        classicPage = TBLClassicPage.init(pageType: "mix", pageUrl: "https://www.google.com", delegate: self, scrollView: nil)
+    }
+}
