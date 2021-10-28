075750b3081d4c7661ef9c382579eebdb8512ffb
Karen Shaham Palman <58982236+KarenShaham@users.noreply.github.com>
2021-10-28 11:49:54 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "TaboolaSDKPackage",
-            targets: ["TaboolaSDKPackage", "TaboolaSDKFramework", "TaboolaSDK"]),
+            targets: ["TaboolaSDKPackage", "ModuleX-ObjC", "ModuleX", "TaboolaSDK"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,13 +19,16 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "TaboolaSDKPackage",
+            name: "ModuleX-ObjC",
             dependencies: [],
-            exclude: ["TaboolaSDKFramework"],
-            publicHeadersPath: "TaboolaSDKFramework"),
+            exclude: [],
+            cSettings: [
+                  .headerSearchPath("TaboolaSDKFramework"),
+               ]),
         .target(
-            name: "TaboolaSDKFramework",
-            dependencies: ["TaboolaSDK"]),
+            name: "ModuleX",
+            dependencies: ["ModuleX-ObjC"],
+            path: "TaboolaSDKPackage"),
         .binaryTarget(
             name: "TaboolaSDK",
             path: "TaboolaSDK.xcframework"
