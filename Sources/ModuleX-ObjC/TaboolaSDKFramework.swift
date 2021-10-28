d189b731797533f539d98ceea816bdf3bf10cfd6
Karen Shaham Palman <58982236+KarenShaham@users.noreply.github.com>
2021-10-28 12:04:56 +0300

    Add files via upload

---
+++ Sources/ModuleX-ObjC/TaboolaSDKFramework.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Karen Shaham Palman on 28/10/2021.
+//
+
+import Foundation
+import TaboolaSDK
+
+module TaboolaSDK.Swift {
+    header "TaboolaSDK-Swift.h"
+    requires objc
+}
